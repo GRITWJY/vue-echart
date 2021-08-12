ac2434ca83b8e84dded73ae0d9055a94bfc884fd
王蒋煜 <1435408225@qq.com>
2021-08-12 20:06:28 +0800

    拆解图表的option,把数据option分离出来

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -26,6 +26,72 @@ export default {
     //初始化
     initChart(){
       this.chartInstance = this.$echarts.init(this.$refs.seller_ref,'chalk')
+
+      //图表初始化配置的控制
+      const initOption = {
+        title:{
+          text:'▎商家销售统计',
+          textStyle:{
+            fontSize:66
+          },
+          left:20,
+          top:20
+        },
+        grid:{
+          top:'20%',
+          left:'3%',
+          right:'6%',
+          bottom:'3%',
+          containLabel:true
+        },
+        xAxis:{
+          type:'value'
+        },
+        yAxis:{
+          type:'category',
+        },
+        tooltip:{
+          trigger:'axis',
+          axisPointer:{
+            type:'line',
+            z:0,
+            lineStyle: {
+              width:66,
+              color:'#2D3443',
+            }
+          }
+        },
+        series:[
+          {
+            type:'bar',
+            barWidth: 66,
+            label: {
+              show:true,
+              position:'right',
+              textStyle:{
+                color:'white'
+              }
+            },
+            itemStyle: {
+              barBorderRadius:[0, 33, 33, 0],
+              //指明方向
+              //指明不同百分比之下的颜色的值
+              color:new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset:0,
+                  color:'#5052EE'
+                },
+                {
+                  offset:1,
+                  color:'#AB6EE5'
+                }
+              ])
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(initOption)
+
       //图表对象鼠标事件监听
       this.chartInstance.on('mouseover',()=>{
         clearInterval(this.timerId)
@@ -56,72 +122,17 @@ export default {
       const sellerValues = showData.map((item)=>{
         return item.value
       })
-      const option = {
-        title:{
-          text:'▎商家销售统计',
-          textStyle:{
-            fontSize:66
-          },
-          left:20,
-          top:20
-        },
-        grid:{
-          top:'20%',
-          left:'3%',
-          right:'6%',
-          bottom:'3%',
-          containLabel:true
-        },
-        xAxis:{
-          type:'value'
-        },
+      const dataOption = {
         yAxis:{
-          type:'category',
           data:sellerNames
-        },
-        tooltip:{
-          trigger:'axis',
-          axisPointer:{
-            type:'line',
-            z:0,
-            lineStyle: {
-              width:66,
-              color:'#2D3443',
-            }
-          }
         },
         series:[
           {
-            type:'bar',
             data:sellerValues,
-            barWidth: 66,
-            label: {
-              show:true,
-              position:'right',
-              textStyle:{
-                color:'white'
-              }
-            },
-            itemStyle: {
-              barBorderRadius:[0, 33, 33, 0],
-              //指明方向
-              //指明不同百分比之下的颜色的值
-              color:new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
-                {
-                  offset:0,
-                  color:'#5052EE'
-                },
-                {
-                  offset:1,
-                  color:'#AB6EE5'
-                }
-              ])
-            }
-
           }
         ]
       }
-      this.chartInstance.setOption(option)
+      this.chartInstance.setOption(dataOption)
     },
     startInterval(){
       if (this.timerId){
