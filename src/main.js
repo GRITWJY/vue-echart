a24ff2dcd006a6f51fee616fb0a670f0351846b3
王蒋煜 <1435408225@qq.com>
2021-08-16 00:19:47 +0800

    定义连接服务器的方法

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import store from './store'
 //引入全局样式文件
 import './assets/css/global.less'
 import './assets/font/iconfont.css'
+import SocketService from "@/utils/socket_service";
 
 axios.defaults.baseURL = 'http://127.0.0.1:8008/api/'
-
+//对服务端进行websocket的连接
+SocketService.Instance().connect()
 Vue.config.productionTip = false
 Vue.prototype.$echarts = window.echarts
 Vue.prototype.$axios = axios
--- src/utils/socket_service.js
+++ src/utils/socket_service.js
@@ -10,4 +10,16 @@ export default class SocketService {
 		}
 		return this.instance
 	}
+
+	//和服务端连接的socket对象
+	ws = null
+
+	//定义连接服务器的方法
+	connect() {
+		//连接服务器
+		if (window.WebSocket) {
+			return console.log("你的浏览器不支持WebSocket")
+		}
+		this.ws = new WebSocket('ws://localhost:9998')
+	}
 }
